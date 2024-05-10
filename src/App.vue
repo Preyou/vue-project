c049618c6ca8507e0491bb4f0fb2b9ca98fc1284
阳文虎 <preyou@foxmail.com>
2024-05-10 15:24:06 +0800

    init🎉:

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VueChart from 'vue-echarts'
+import {ref} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const a = ref()
+console.log(a);
+
+const b = ref()
+console.log(b);
+</script>
+
+<template>
+  <VueChart ref="a"></VueChart>
+  <HelloWorld ref="b" />
+</template>
