c482e9a4db6a5220884b7fb74fe48bcfc94530cc
Preyou <preyou@foxmail.com>
2024-06-17 10:50:53 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@ const name = ref('aaaa')
 <template>
 <input v-model="name"></input>
 <NTabs style="width:100vw;height:100vh" placement="left" type="card" closable addable>
+  <template #prefix>
+      Prefix
+  </template>
   <NTabPane name="a" tab="a">
     <template #tab>
     {{name}}
     </template>
   </NTabPane>
+  <template #suffix>
+      Suffix
+  </template>
 </NTabs>
 </template>
